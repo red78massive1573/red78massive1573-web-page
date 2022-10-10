aba2f28429b75f02df27bc066bd3bf1f27453a95
red78massive1573 <87458936+red78massive1573@users.noreply.github.com>
2022-10-10 21:20:42 +0700

    Delete audioPlayer directory

--- audioPlayer/main.js
+++
@@ -1,4 +0,0 @@
-var audio=new Audio();audio.preload='none';var hasErrors;var lastUrl;var canPause;function play(urlId,loaderId,slug){var url=document.getElementById(urlId).textContent;if(canPause){audio.pause();audio.currentTime=0;}
-if(lastUrl!=url||hasErrors){audio.src=url;lastUrl=url;hasErrors=false;canPause=false;audio.onerror=function(){hasErrors=true};if(loaderId){document.getElementById(loaderId).style.display='block';audio.addEventListener('canplay',_=>{document.getElementById(loaderId).style.display='none';});}}
-var playPromise=audio.play();if(playPromise!==undefined){playPromise.then(_=>{canPause=true;}).catch(error=>{hasErrors=true;});}
-if(navigator.vibrate){navigator.vibrate(5);}}
